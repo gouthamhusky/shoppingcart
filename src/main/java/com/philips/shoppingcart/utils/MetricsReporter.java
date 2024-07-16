6142803383442bfcdd4656e831b5e2668347f1e2
Goutham K <kanahasabai.g@northeastern.edu>
2024-07-16 06:45:05 -0400

    feat: updated MetricsReporter definition

--- src/main/java/com/philips/shoppingcart/utils/MetricsReporter.java
+++ src/main/java/com/philips/shoppingcart/utils/MetricsReporter.java
@@ -3,20 +3,19 @@ package com.philips.shoppingcart.utils;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MetricsReporter {
 
-    @Value("${spring.application.name}")
-    private String appName;
+    private final StatsDClient statsd;
 
-    @Value("${cart.webapp.statsd.client.host}")
-    private String host;
-
-
-    private final StatsDClient statsd = new NonBlockingStatsDClient(appName, host, 8125);
+    public MetricsReporter(@Value("${spring.application.name}") String appName,
+                           @Value("${cart.webapp.statsd.client.host}") String host) {
+        this.statsd = new NonBlockingStatsDClient(appName, host, 8125);
+    }
 
     public void recordCounter(HttpStatus status){
         String counterName = new StringBuilder().append(status.toString())
